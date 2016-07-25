c184a5b64b8ee4ff5cc39f7c2ac326448a67549d
Sandra Tsai <sandraytsai@gmail.com>
2016-07-25 17:51:13 -0400

    command 1

--- contact_list.rb
+++ contact_list.rb
@@ -6,3 +6,9 @@ class ContactList
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
 
 end
+
+puts "Here is a list of available commands:
+\tnew - Create a new contact
+\tlist - List all ContactList
+\tshow - Show a contact
+\tsearch - Search contacts"
